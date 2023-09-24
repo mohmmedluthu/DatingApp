3b5ac4e160381b0cca4912b97956d25d958d616f
mohmmedluthu <ltfking213@gmail.com>
2023-09-24 08:31:29 +0530

    End of section2

---
+++ API/-- SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Luffy");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Zoro");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Sanji");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4, "Gojo");
